1cde06b7c1df528e24054a8f78da77dfeda1e237
José Carlos <jc443559@gmail.com>
2026-05-05 18:39:21 -0300

    Reformulando a Rota de login

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
  * Rota inicial / dashboard
  */
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 })->name('welcome');
 
 /**
